fd4995f8e2808b7ce0cce33728139c7cd7081d53
ktr92 <kudashevtim@gmail.com>
2021-08-24 16:47:19 +0300

    start

--- js/main.js
+++ js/main.js
@@ -48,15 +48,16 @@ $(document).ready(function () {
       horizontal: 1,
       itemNav: 'basic',
       smart: 1,
-      activateOn: 'click',
+      activateOn: 'mouseenter',
       mouseDragging: 1,
       touchDragging: 1,
       releaseSwing: 1,
       startAt: 0,
       scrollBar: $wrap.find('.scrollbar'),
       scrollBy: 1,
-      speed: 1000,
+      speed: 400,
       elasticBounds: 1,
+      easing: 'swing',
       dragHandle: 1,
       dynamicHandle: 1,
       clickBar: 1,
@@ -68,9 +69,179 @@ $(document).ready(function () {
 
       // Buttons
       prev: $wrap.find('.productslider__arrow_left'),
-      next: $wrap.find('.productslider__arrow_right')
+      next: $wrap.find('.productslider__arrow_right'),
+    });
+
+    $('.filter__header-js').on('click', function() {
+      $(this).parent().toggleClass('filter__item_open');
+      $(this).siblings('.filter__form').slideToggle();
+    });
+
+   
+});
+
+
+
+window.onload = function() {
+
+if ($("#sliderprice1").length) {
+
+  $("#maxCost1").val($('#limitMax1').val());
+  $("#minCost1").val($('#limitMin1').val());
+  
+
+ /* function prettify(num) {
+    var n = num.toString();
+    var separator = " ";
+    return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + separator);
+  }
+
+  var $test1 = $("#maxCost1");
+  var vals1 = $test1.prop("value");
+  $test1.prop("value", prettify(vals1));
+
+  var $test2 = $("#minCost1");
+  var vals2 = $test2.prop("value");
+  $test2.prop("value", prettify(vals2));*/
+  
+  try {
+
+
+    jQuery("#sliderprice1").slider({
+      min: +jQuery("input#limitMin1").val(),
+      max: +jQuery("input#limitMax1").val(),
+      values: [+jQuery("input#limitMin1").val(), +jQuery("input#limitMax1").val()],
+      range: "min",
+      stop: function (event, ui) {
+        jQuery("input#minCost1").val(jQuery("#sliderprice1").slider("values",0));
+        jQuery("input#maxCost1").val(jQuery("#sliderprice1").slider("values",1));
+
+        //var val1 = $test1.prop("value");
+        //$test1.prop("value", prettify(val1));
+
+        if (jQuery("input#minCost1").val() == $('#limitMin1').val()) {
+          jQuery("input#minCost1").addClass('notchanged');
+        }
+        else {
+          jQuery("input#minCost1").removeClass('notchanged');
+        }
+
+        if (jQuery("input#maxCost1").val() == $('#limitMax1').val()) {
+          jQuery("input#maxCost1").addClass('notchanged');
+        }
+        else {
+          jQuery("input#maxCost1").removeClass('notchanged');
+        }
+
+        
+      },
+      slide: function (event, ui) {
+        jQuery("input#minCost1").val(jQuery("#sliderprice1").slider("values",0));
+        jQuery("input#maxCost1").val(jQuery("#sliderprice1").slider("values",1));
+
+        if (jQuery("input#minCost1").val() == $('#limitMin1').val()) {
+          jQuery("input#minCost1").addClass('notchanged');
+        }
+        else {
+          jQuery("input#minCost1").removeClass('notchanged');
+        }
+
+        if (jQuery("input#maxCost1").val() == $('#limitMax1').val()) {
+          jQuery("input#maxCost1").addClass('notchanged');
+        }
+        else {
+          jQuery("input#maxCost1").removeClass('notchanged');
+        }
+
+        
+        //var val1 = $test1.prop("value");
+       // $test1.prop("value", prettify(val1));
+      }
+    });
+
+  
+
+
+    jQuery("input#maxCost1").change(function () {
+
+      var value1=jQuery("input#minCost1").val();
+      var value2=jQuery("input#maxCost1").val();
+
+      if (value2 > (+jQuery("input#limitMax1").val())) {
+        value2 = +jQuery("input#limitMax1").val();
+        jQuery("input#maxCost1").val(+jQuery("input#limitMax1").val())
+      }
+
+     
+      jQuery("#sliderprice1").slider("values", 1, value2);
+      $test1.prop("value", prettify(val1));
+      var val1 = $test1.prop("value");
+
+        $test1.prop("value", prettify(val1));
+
+        if (jQuery("input#minCost1").val() == $('#limitMin1').val()) {
+          jQuery("input#minCost1").addClass('notchanged');
+        }
+        else {
+          jQuery("input#minCost1").removeClass('notchanged');
+        }
+
+        if (jQuery("input#maxCost1").val() == $('#limitMax1').val()) {
+          jQuery("input#maxCost1").addClass('notchanged');
+        }
+        else {
+          jQuery("input#maxCost1").removeClass('notchanged');
+        }
+    
+    });
+
+     jQuery("input#minCost1").change(function () {
+
+      var value1=jQuery("input#minCost1").val();
+      var value2=jQuery("input#maxCost1").val();
+
+      if (value2 < (+jQuery("input#limitMin1").val())) {
+        value2 = +jQuery("input#limitMin1").val();
+        jQuery("input#minCost1").val(+jQuery("input#limitMin1").val())
+      }
+
+     
+      jQuery("#sliderprice1").slider("values", 0, value1);
+      var val1 = $test1.prop("value");
+      $test1.prop("value", prettify(val1));
+
+      var val1 = $test1.prop("value");
+        $test1.prop("value", prettify(val1));
+
+        if (jQuery("input#minCost1").val() == $('#limitMin1').val()) {
+          jQuery("input#minCost1").addClass('notchanged');
+        }
+        else {
+          jQuery("input#minCost1").removeClass('notchanged');
+        }
+
+        if (jQuery("input#maxCost1").val() == $('#limitMax1').val()) {
+          jQuery("input#maxCost1").addClass('notchanged');
+        }
+        else {
+          jQuery("input#maxCost1").removeClass('notchanged');
+        }
+    
     });
 
 
-});
+
+
+  } catch (err) {
+
+  };
+
+
+}
+
+};
+
+
+
+
 
